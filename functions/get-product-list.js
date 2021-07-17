e74daa251b310a1f846c59136c6e2fcccfa53b9e
Juan P. Prieto <jp@calltheguys.co>
2021-07-17 10:13:40 -0700

    add caching  + layout

--- functions/get-product-list.js
+++ functions/get-product-list.js
@@ -16,64 +16,77 @@
  */
 
 const { postToShopify } = require('./utils/postToShopify')
+const Cache = require('./utils/Cache')
 
 exports.handler = async () => {
   try {
     console.log('--------------------------------')
     console.log('Retrieving product list...')
     console.log('--------------------------------')
-    const shopifyResponse = await postToShopify({
-      query: `
-        query getProductList {
-          products(sortKey: TITLE, first: 100) {
-            edges {
-              node {
-                id
-                handle
-                description
-                title
-                totalInventory
-                variants(first: 5) {
-                  edges {
-                    node {
-                      id
-                      title
-                      quantityAvailable
-                      priceV2 {
+
+    if (!Cache.has('getProductList')) {
+        const shopifyResponse = await postToShopify({
+          query: `
+            query getProductList {
+              products(sortKey: TITLE, first: 100) {
+                edges {
+                  node {
+                    id
+                    handle
+                    description
+                    title
+                    totalInventory
+                    variants(first: 5) {
+                      edges {
+                        node {
+                          id
+                          title
+                          quantityAvailable
+                          priceV2 {
+                            amount
+                            currencyCode
+                          }
+                        }
+                      }
+                    }
+                    priceRange {
+                      maxVariantPrice {
+                        amount
+                        currencyCode
+                      }
+                      minVariantPrice {
                         amount
                         currencyCode
                       }
                     }
-                  }
-                }
-                priceRange {
-                  maxVariantPrice {
-                    amount
-                    currencyCode
-                  }
-                  minVariantPrice {
-                    amount
-                    currencyCode
-                  }
-                }
-                images(first: 1) {
-                  edges {
-                    node {
-                      src
-                      altText
+                    images(first: 1) {
+                      edges {
+                        node {
+                          src
+                          altText
+                        }
+                      }
                     }
                   }
                 }
               }
             }
-          }
-        }
-      `,
-    })
+          `,
+        })
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(shopifyResponse),
+      Cache.set('getProductList', shopifyResponse)
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(shopifyResponse),
+      }
+    } else {
+      const shopifyResponse = Cache.get('getProductList')
+      console.log('Using cached products response')
+      return {
+        statusCode: 200,
+        body: JSON.stringify(shopifyResponse),
+      }
     }
   } catch (error) {
     console.log(error)
--- functions/get-product.js
+++ functions/get-product.js
@@ -14,65 +14,79 @@
  */
 
 const { postToShopify } = require('./utils/postToShopify')
+const Cache = require('./utils/Cache')
 
 exports.handler = async (event) => {
-  const { itemHandle } = JSON.parse(event.body)
+  // const { itemHandle } = JSON.parse(event.body)
+  const handle = event?.queryStringParameters?.handle
 
   try {
     console.log('--------------------------------')
     console.log('Retrieving product details...')
     console.log('--------------------------------')
-    const shopifyResponse = await postToShopify({
-      query: `
-        query getProduct($handle: String!) {
-          productByHandle(handle: $handle) {
-            id
-            handle
-            description
-            title
-            totalInventory
-            variants(first: 5) {
-              edges {
-                node {
-                  id
-                  title
-                  quantityAvailable
-                  priceV2 {
-                    amount
-                    currencyCode
+    if (!Cache.has(`getProduct-${handle}`)) {
+      const shopifyResponse = await postToShopify({
+        query: `
+          query getProduct($handle: String!) {
+            productByHandle(handle: $handle) {
+              id
+              handle
+              description
+              title
+              totalInventory
+              variants(first: 5) {
+                edges {
+                  node {
+                    id
+                    title
+                    quantityAvailable
+                    priceV2 {
+                      amount
+                      currencyCode
+                    }
+                  }
+                }
+              }
+              priceRange {
+                maxVariantPrice {
+                  amount
+                  currencyCode
+                }
+                minVariantPrice {
+                  amount
+                  currencyCode
+                }
+              }
+              images(first: 2) {
+                edges {
+                  node {
+                    src
+                    altText
                   }
                 }
               }
             }
-            priceRange {
-              maxVariantPrice {
-                amount
-                currencyCode
-              }
-              minVariantPrice {
-                amount
-                currencyCode
-              }
-            }
-            images(first: 1) {
-              edges {
-                node {
-                  src
-                  altText
-                }
-              }
-            }
           }
-        }
-      `,
-      variables: {
-        handle: itemHandle,
-      },
-    })
+        `,
+        variables: {
+          handle: handle,
+        },
+      })
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(shopifyResponse),
+      Cache.set(`getProduct-${handle}`, shopifyResponse.productByHandle)
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(shopifyResponse.productByHandle),
+      }
+    } else {
+      const cachedProductResponse = Cache.get(`getProduct-${handle}`)
+      console.log('Using cached product', cachedProductResponse)
+      return {
+        statusCode: 200,
+        body: JSON.stringify(cachedProductResponse),
+      }
+
     }
   } catch (error) {
     console.log(error)
